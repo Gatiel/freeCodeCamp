420361ffe1d119fda7d128e4fb400565db2c692e
Dr Gatiel <69369161+Gatiel@users.noreply.github.com>
2024-01-25 12:57:58 -0300

    Update find-the-symmetric-difference.js

--- find-the-symmetric-difference.js
+++ find-the-symmetric-difference.js
@@ -1,6 +1,7 @@
 function sym() {
+  //cria uma lista vazia
   const args = [];
-
+  //loop para percorrer todos os argumentos e empurrar para a lista vazia args
   for (let i = 0; i < arguments.length; i++){
     args.push(arguments[i]);
   }
@@ -8,13 +9,13 @@ function sym() {
 
 function symDiff(arrOne, arrTwo){
   const results = [];
-
+  //analisa o arrOne, ve se tem não tem no arrTwo e se não tem na lista da variavel results, se tiver, empurra para results
   arrOne.forEach(function (item) {
     if (arrTwo.indexOf(item) < 0 && results.indexOf(item) < 0){
       results.push(item);
     }
   });
-
+  //realiza o mesmo processo que anterior, mas para o arrTwo, analisando para o arrOne e results, empurrando para results
   arrTwo.forEach(function (item) {
     if (arrOne.indexOf(item) < 0 && results.indexOf(item) < 0){
       results.push(item);
@@ -23,6 +24,6 @@ function symDiff(arrOne, arrTwo){
 
   return results;
 }
-
+  //concatena todos os valores com diferença assimetrica
 return args.reduce(symDiff);
 }
